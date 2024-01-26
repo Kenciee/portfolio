c677833539baa445ea4cde72e1524265ce9567d6
Kenciee <153866159+Kenciee@users.noreply.github.com>
2024-01-26 11:42:47 +0600

    Add files via upload

---
+++ pages/Projects.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Project from '../components/project/Project';
+import {projects} from "./../helpers/ProjectsList.js"
+
+
+import project02 from './../img/projects/02.jpg';
+import project03 from './../img/projects/03.jpg';
+import project04 from './../img/projects/04.jpg';
+import project05 from './../img/projects/05.jpg';
+import project06 from './../img/projects/06.jpg';
+
+
+const Projects = () => {
+    return (
+        <main className="section">
+        <div className="container">
+            <h2 className="title-1">Projects</h2>
+            <ul className="projects">
+                
+                {projects.map((project, index) => {
+                        return  <Project key={index} 
+                        title={project.title} 
+                        img={project.img}
+                        index = {index}
+                        />
+                        
+                })}
+                
+                <Project />  
+                <Project />  
+                <Project />  
+
+
+            </ul>
+        </div>
+    </main>
+    );
+};
+
+export default Projects;
